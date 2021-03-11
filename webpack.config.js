afbba55ca302911f27df815acdc2aa97dec99840
CFCode-git <435184766@qq.com>
2021-03-11 09:54:12 +0800

    使用 babel-loader 打包 js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  mode: 'production'
+  mode: 'production',
+  module:{
+    rules: [
+      {
+        test:/\.jsx?$/,
+        exclude: /node_modules/, // 排除
+        use:{
+          loader: 'babel-loader',
+          options:{
+            presets:[ // 预先规则
+              '@babel/preset-env'
+            ]
+          }
+        }
+      }
+    ]
+  }
 }
