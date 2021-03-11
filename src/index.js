c9ab7fa367dd2d807c24880840c0dfc0277347d5
CFCode-git <435184766@qq.com>
2021-03-11 09:45:20 +0800

    支持 IE, 不完全支持, 因为 IE 没有 Promise, 需要添加 polyfill

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-const str = 'hi'
-console.log(str)
+import {a} from './a.js'  // 直接引入
+const b = import ('./b') // 动态引入, 按需加载
+
+const hi = ()=>{
+  console.log('QY')
+  console.log(a)
+  console.log(b)
+  console.log(Promise.resolve('test promise')) // IE 不支持 ,需要添加 polyfill
+}
+
+hi()
