7c61b0ac70116f39f835eda8de56123dd00c9892
CFCode-git <435184766@qq.com>
2021-03-11 10:30:39 +0800

    支持 TSX

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import {a} from './a.js' // 动态引入, 按需加载
 import {JsxDemo} from './jsx-demo.jsx'  // 直接引入
+import {TsxDemo} from './tsx-demo.tsx'  // 直接引入
 import {x} from './ts-demo.ts'
 const b = import ('./b')
 
 console.log(x)
 console.log(JsxDemo)
+console.log(TsxDemo)
 
 const hi = ()=>{
   console.log('QY')
