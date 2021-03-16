f98ec907473055a34595a1845ef715b9ad9690f7
CFCode-git <435184766@qq.com>
2021-03-16 12:54:30 +0800

    支持 scss 文件

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {a} from 'src/a.js'
 import {JsxDemo} from './jsx-demo.jsx'
 import {TsxDemo} from './tsx-demo.tsx'
 import {x} from './ts-demo.ts'
+import 'src/scss-demo.scss'
 const b = import ('./b') // 动态引入, 按需加载
 
 console.log(x)
